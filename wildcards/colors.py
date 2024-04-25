a5d4b159a0972823e3720151fa2c18d348399196
Tom Amaral <thomasamaral2016@gmail.com>
2024-04-24 19:09:04 -0700

    init

---
+++ wildcards/colors.py
@@ -0,0 +1,19 @@
+COLORS = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "orange",
+    "purple",
+    "pink",
+    "black",
+    "white",
+    "brown",
+    "tan",
+    "gray"
+]
+
+COLOR_MODS = [
+    "light",
+    "dark"
+]
