8ac58331c5221b078fb4bcffb710c0e8f373c4a2
Tom Amaral <thomasamaral2016@gmail.com>
2024-06-13 19:39:23 -0700

    make pose optional, resolution sizing fix, copy prompt to output dir

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from PIL import Image
 import requests
 import sys
 import os
 import json
 import base64
 import random
+import shutil
+
+from PIL import Image
 from datetime import datetime
 from io import BytesIO
 from app_config import SD_API_URL, SD_API_PORT
@@ -107,7 +109,6 @@ class DatasetBuilder:
                 if "full" in path:
                     self.full_ref = ReferenceImage(path)
                 elif "half" in path:
-                    print(path)
                     self.half_ref = ReferenceImage(path)
                 elif "bust" in path:
                     self.bust_ref = ReferenceImage(path)
@@ -129,7 +130,7 @@ class DatasetBuilder:
             self, expression: str, 
             background: str,
             reference: ReferenceImage,
-            pose: ReferenceImage,
+            pose: ReferenceImage|None,
             style: str) -> dict:
         """
         Given expression and background prompt snippets, construct the full payload
@@ -137,18 +138,19 @@ class DatasetBuilder:
         controlnet module payload objects.
         """
         # config value TODO
-        desired_prompt_res = 960
+        # the short side
+        desired_prompt_res = 512
         
         # Get ref image dimensions and aspect ratio.
         ref_height = reference.image().height
         ref_width = reference.image().width
         
         if ref_height > ref_width:
-            prompt_height = desired_prompt_res
-            prompt_width = (ref_width * desired_prompt_res) / ref_height
-        else:
             prompt_width = desired_prompt_res
             prompt_height = (ref_height * desired_prompt_res) / ref_width
+        else:
+            prompt_height = desired_prompt_res
+            prompt_width = (ref_width * desired_prompt_res) / ref_height
         
 
         # TODO: This should be configurable :P
@@ -170,8 +172,12 @@ class DatasetBuilder:
             num_artists = random.randint(2, 6)
             for _ in range(0, num_artists):
                 style += f"{ARTISTS[random.randint(0, len(ARTISTS) - 1)]}, "
-    
-        pose_tags = pose.name().split(".")[0]
+
+        if pose:
+            pose_tags = pose.name().split(".")[0]
+        else:
+            pose_tags = ""
+
         payload["prompt"] = f"{self.base_positive}, {expression}, ({pose_tags}), {background}, {style}, (best quality, masterpiece)"
         payload["negative_prompt"] = f"{self.base_negative}, {COMMON_NEG}"
 
@@ -181,11 +187,7 @@ class DatasetBuilder:
         ref_bytes = ref_file.getvalue()
         ref_b64 = base64.b64encode(ref_bytes).decode('utf-8')
 
-        # convert pose reference image to bytes
-        pose_file = BytesIO()
-        pose.image().save(pose_file, "png")
-        pose_bytes = pose_file.getvalue()
-        pose_b64 = base64.b64encode(pose_bytes).decode('utf-8')
+        
 
         # Controlnet 
         payload["alwayson_scripts"] = {
@@ -229,27 +231,38 @@ class DatasetBuilder:
                         "use_preview_as_input": False,
                         "weight": 0.25
                     },
-                    {
-                        "control_mode": "ControlNet is more important",
-                        "enabled": True,
-                        "guidance_end": 0.6,    # variable
-                        "guidance_start": 0,
-                        "hr_option": "Both",
-                        "image": pose_b64,
-                        "input_mode": "simple",
-                        "model": "control_v11p_sd15_openpose [cab727d4]",
-                        "module": "None",
-                        "pixel_perfect": True,
-                        # "processor_res": 512,
-                        "resize_mode": "Just Resize",
-                        "save_detected_map": False,
-                        "threshold_a": 0.5,
-                        "threshold_b": 0.5,
-                        "weight": 1  # variable
-                    }
+                    
                 ]
             }
         }
+
+        if pose:
+            # convert pose reference image to bytes
+            pose_file = BytesIO()
+            pose.image().save(pose_file, "png")
+            pose_bytes = pose_file.getvalue()
+            pose_b64 = base64.b64encode(pose_bytes).decode('utf-8')
+
+            payload["alwayson_scripts"]["ControlNet"]["args"].append(
+                {
+                    "control_mode": "ControlNet is more important",
+                    "enabled": True,
+                    "guidance_end": 0.6,    # variable
+                    "guidance_start": 0,
+                    "hr_option": "Both",
+                    "image": pose_b64,
+                    "input_mode": "simple",
+                    "model": "control_v11p_sd15_openpose [cab727d4]",
+                    "module": "None",
+                    "pixel_perfect": True,
+                    # "processor_res": 512,
+                    "resize_mode": "Just Resize",
+                    "save_detected_map": False,
+                    "threshold_a": 0.5,
+                    "threshold_b": 0.5,
+                    "weight": 1  # variable
+                }
+            )
 
         return payload
 
@@ -265,14 +278,12 @@ class DatasetBuilder:
 
         if poses:
             # load pose collection for the desired shot length
-            pose_imgs = []
+            # Prepend a 'None' so that the pose isn't applied for one iteration
+            pose_imgs = [None]
 
             # load all file names
             file_paths = [f"./poses/{poses}/{f.lower()}" for f in os.listdir(f"./poses/{poses}") 
                      if os.path.isfile(os.path.join(f"./poses/{poses}", f))]
-            
-            print(file_paths)
-
 
             for pose_path in file_paths:
                 pose_imgs.append(ReferenceImage(pose_path))
@@ -310,15 +321,19 @@ class DatasetBuilder:
         self.run_output_path = os.path.join(run_output_dir, f"{datetime.now()}")
         os.mkdir(self.run_output_path)
 
+        # copy prompt.json from project_dir into output dir
+        shutil.copyfile(
+            os.path.join(self.project_dir, "prompt.json"),
+            os.path.join(self.run_output_path, "prompt.json"))
 
         # build and execute prompts for each reference
         
-        # if self.full_ref.image() is not None:
-        #     # create output directory for this reference if not exists
-        #     output_path = os.path.join(self.run_output_path, "FULL")
-        #     os.mkdir(output_path)
+        if self.full_ref.image() is not None:
+            # create output directory for this reference if not exists
+            output_path = os.path.join(self.run_output_path, "FULL")
+            os.mkdir(output_path)
 
-        #     self.reference_workflow(self.full_ref, output_path, poses="FULL")
+            self.reference_workflow(self.full_ref, output_path, poses="FULL")
 
         if self.half_ref.image() is not None:
             # create output directory for this reference if not exists
